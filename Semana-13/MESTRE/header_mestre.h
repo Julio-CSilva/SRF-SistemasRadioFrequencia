509cb70bfcdbfd29b62a367da8a99d36155b0213
Júlio Silva <juliocesardasilvafilho@hotmail.com>
2021-09-03 14:47:52 -0300

    Create header_mestre.h
    Master.c

---
+++ Semana-13/MESTRE/header_mestre.h
@@ -0,0 +1,23 @@
+//******************************************************************************
+// IMD - Instituto Metropole Digital 
+// Disciplina: Projetos de Sistemas RF
+// Aula 09: Transmissão de dados via protocolo SPI
+// Autor: Ellon Paiva Mendes
+// Professor Revisor: Heitor Florencio
+
+/** D E C L A R A Ç Õ E S **************************************************/
+// Nenhuma
+
+/** D E F I N I Ç Õ E S ****************************************************/
+// Nenhuma
+
+/** E X T E R N A S ************************************************************/
+// Declare aqui as variáveis acessíveis por outros arquivos
+// Nenhuma
+
+/** P R O T Ó T I P O S ******************************************************/
+
+void InterruptServiceHigh(void); //Serviço de tratamento de interrupção
+
+void UserInit(void); //Inicialização do PIC
+void UserProcess(void); // Processamento principal do PIC
